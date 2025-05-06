746e821a7d17deb6a59130e50add6a0daa8782bc
sina <sina.eslami99@gmail.com>
2025-05-06 18:47:33 +0330

    addtoCart

--- lesson-13/scripts/amazon.js
+++ lesson-13/scripts/amazon.js
@@ -59,32 +59,25 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
+      
       const productId = button.dataset.productId;
-
-      let matchingItem;
+      let matchingItems;
 
       cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
+        if (productId === item.productId){
+          matchingItems = item;
         }
       });
 
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1
+      if (matchingItems){
+        matchingItems.quantity+1;
+      } else{
+          cart.push({
+            productId : productId,
+            quantity: 1
         });
+        console.log(cart);
       }
-
-      let cartQuantity = 0;
-
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
+     
     });
   });
